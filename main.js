2503aff7844677c6bbbcab2ecfa9fd8bde9e4b1b
lessuiz <leonardoparreira2018@gmail.com>
2020-05-25 03:20:56 -0300

    Added operations funcionality

--- main.js
+++ main.js
@@ -1,13 +1,43 @@
 const numButtons = document.querySelectorAll(".numButton")
+const operationButtons = document.querySelectorAll(".operation-button")
+const equalsButton = document.querySelectorAll("#equals")
+const clearButton = document.querySelector("#clear")
+const deleteButton = document.querySelector("#delete")
 let display = document.querySelector("#result")
+let previousResult = document.querySelector("#previous")
 let value = "";
+let num1 = 0
+let num2 = 0
+let operationsQuery = []
 
 //** Loop to create event listeners **//
-buttons.forEach(button => {
+numButtons.forEach(button => {
   button.addEventListener('click', () => {
-    value += numButton.textContent
+    value += button.textContent
     display.textContent = value
   })
+})
+
+//** This function pushes inputs to the operation array **//
+function pushNumbers(e) {
+  operationsQuery.push(parseInt(display.textContent))
+  if (operationsQuery.length === 3) {
+    operationsQuery.splice(0, 3, operate(operationsQuery[1], operationsQuery[0], operationsQuery[2]))
+    if (operationsQuery[0] == Infinity) {
+      operationsQuery = []
+      previousResult.textContent = "Infinity"
+      display.textContent = ""
+      return
+    }
+  }
+  operationsQuery.push(e.target.textContent)
+  previousResult.textContent = operationsQuery[0]+' '+operationsQuery[1]
+  value = ""
+  display.textContent = value
+}
+
+operationButtons.forEach(button => {
+  button.addEventListener('click', pushNumbers)
 })
 
 //** Algebric functions **//
